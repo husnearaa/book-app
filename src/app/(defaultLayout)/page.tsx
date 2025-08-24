d74d962819392758ee85021cc71fdd8875c62468
husnearaa <hosnearapopy462@gmail.com>
2025-08-24 12:35:52 +0600

    books page has render dynamically using userouter and usepathname react hook

--- src/app/(defaultLayout)/page.tsx
+++ src/app/(defaultLayout)/page.tsx
@@ -1,13 +1,28 @@
 // import BookChapters from "@/components/Book/BookChapters";
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+
+const router = useRouter();
+const pathname = usePathname();
+
+useEffect(() => {
+  if (pathname === "/") {
+    router.push("/books");
+  }
+}, [pathname, router]);
+
+
+  // return (
+  //   <div>
+  //     <h1>Home</h1>
+  //   </div>
+  // );
 };
 
 export default HomePage;
